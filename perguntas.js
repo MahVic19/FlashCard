c604d06b94c90a0ebac6b47447f0bdbb9b0043c9
MahVic19 <mariavictoriaestevam53@gmail.com>
2024-11-08 21:51:40 -0300

    arquivando cartões

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o nome do hormônio que regula o sono?',
+    'É a melatonina'
+)
+
+criaCartao(
+    'Geográfia',
+    'Qual é o maior oceano da Terra?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz maçã em Inglês?',
+    'Maçã em inglês é Apple (Épou)'
+)
